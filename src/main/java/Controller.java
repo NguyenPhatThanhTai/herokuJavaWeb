5071bc718995f4bc1c522c019744e833ce3f5a4f
NguyenPhatThanhTai <57514749+NguyenPhatThanhTai@users.noreply.github.com>
2021-05-25 20:59:01 +0700

    ok

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Base64;
 
-@WebServlet(urlPatterns = "/Tra-Cuu")
+@WebServlet(name="Tra-Cuu",urlPatterns={"/Tra-Cuu"})
 public class Controller extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
