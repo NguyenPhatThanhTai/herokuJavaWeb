ebc89726970f9575f2808bdd86ed35b9ae8bff1b
NguyenPhatThanhTai <57514749+NguyenPhatThanhTai@users.noreply.github.com>
2021-05-25 22:30:06 +0700

    2

--- src/main/java/com/javamaster/controller/Controller.java
+++ src/main/java/com/javamaster/controller/Controller.java
@@ -16,7 +16,7 @@ import java.util.Base64;
 public class Controller extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/View/view/tra-cuu.jsp").forward(req, resp);
+        req.getRequestDispatcher("View/view/tra-cuu.jsp").forward(req, resp);
     }
 
     @Override
